bc922e211e20d883ab3582981328bfa56cd347a9
crazywriter1 <53251494+crazywriter1@users.noreply.github.com>
2023-05-03 12:26:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('pfbFrom');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const ipAddress = document.getElementById('ipAddress').value;
+        const gasLimit = document.getElementById('gasLimit').value;
+        const feeInput = document.getElementById('fee').value;
+        const nameSpaceId = document.getElementById('nameId').value;
+        const data = document.getElementById('data').value;
+
+        // Perform validation or further processing logic
+        if (ipAddress === '') {
+            alert('Please enter your ip address.');
+            return;
+        }
+
+        if (gasLimit === '') {
+            alert('Please enter your gas limit.');
+            return;
+        }
+
+        if (feeInput === '') {
+            alert('Please enter your fee.');
+            return;
+        }
+
+        if (nameSpaceId === '') {
+            alert('Please enter your nameSpaceId.');
+            return;
+        }
+
+        if (data === '') {
+            alert('Please enter your data.');
+            return;
+        }
+
+        const url = `http://${ipAddress}:26659/submit_pfb`;
+        const payload = {
+            namespace_id: nameSpaceId,
+            data: data,
+            gas_limit: parseInt(gasLimit),
+            fee: parseInt(feeInput)
+        };
+        const requestOptions = {
+            method: 'POST',
+            type:'text/plain;charset=utf-8',
+            body: JSON.stringify(payload)
+        };
+
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Response:', data);
+                // Handle the response data as needed
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle the error
+            });
+        console.log(ipAddress,gasLimit,feeInput,nameSpaceId,data)
+        // ... perform validation for other fields if needed ...
+
+
+        // You can also submit the form programmatically using the following line:
+        // form.submit();
+    });
+
+    function generateRandHexEncodedNamespaceID() {
+        const nID = new Uint8Array(8);
+        crypto.getRandomValues(nID);
+        return Array.from(nID).map((byte) => byte.toString(16).padStart(2, '0')).join('');
+    }
+
+    function generateRandMessage() {
+        const lenMsg = Math.floor(Math.random() * 101);
+        const msg = new Uint8Array(lenMsg);
+        crypto.getRandomValues(msg);
+        return Array.from(msg).map((byte) => byte.toString(16).padStart(2, '0')).join('');
+    }
+
+    function generateRandomData() {
+        let seed = 5405;
+        const prevRandom = Math.random; // Save the previous Math.random function
+
+        // Define a new random number generator based on the seed
+        Math.random = function () {
+            const x = Math.sin(seed++) * 10000;
+            return x - Math.floor(x);
+        };
+
+        const nID = generateRandHexEncodedNamespaceID();
+        const msg = generateRandMessage();
+
+        document.getElementById('nameId').value = nID;
+        document.getElementById('data').value = msg;
+        Math.random = prevRandom; // Restore the original Math.random function
+    }
+
+// Add a click event listener to the button
+    const button = document.getElementById('generateButton');
+    button.addEventListener('click', generateRandomData);
+
+});
